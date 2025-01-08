8d4bad9ce514c6ce9579961ba37a9541a9057e38
Sriram8055 <sriramakrishnan8055@gmail.com>
2025-01-08 11:08:22 +0530

    added host in app3.py

--- backend/app3.py
+++ backend/app3.py
@@ -229,4 +229,4 @@ def qa_bot():
         return jsonify({"error": f"Failed to process the question: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True,host="0.0.0.0", port=5001)
